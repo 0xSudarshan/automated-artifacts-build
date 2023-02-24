cd6f93dfe1cabcb3f7b43a532252723bd62f46cd
0xSchierke <103627996+0xSudarshan@users.noreply.github.com>
2023-02-25 00:46:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,52 +7,39 @@ const octokit = new Octokit({
     auth:'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'
   })
   
-//   const {data} =  await octokit.request('GET /repos/{owner}/{repo}/issues?labels=agenda', {
-//     owner: '0xSudarshan',
-//     repo: '0xnode',
-//     headers: {
-//       'X-GitHub-Api-Version': '2022-11-28'
-//     }
-//   })
+  const {data} =  await octokit.request('GET /repos/{owner}/{repo}/issues?labels=agenda', {
+    owner: '0xSudarshan',
+    repo: '0xnode',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
 
-// const workMeetings =  await octokit.request('GET /repos/{owner}/{repo}/issues?labels=Working%20Meetings&per_page=1', {
-//     owner: '0xSudarshan',
-//     repo: 'automated-artifacts-buidl',
-//     headers: {
-//       'X-GitHub-Api-Version': '2022-11-28'
-//     }
-//   })
-//   let bodys  = workMeetings.data[0].body
-//   console.log(JSON.stringify(body))
-//   for (let i =0 ; i < data.length ; i++){
-//     let url = data[i].html_url
-//     let author = data[i].user.login
-//     let startIndex  = parseInt(JSON.stringify(body.indexOf('\r\n| -- | -- | -- |\r\n'))) 
-//     console.log(startIndex)
-//     let lastIndex  = startIndex+19
-//     console.log(lastIndex)
-//     let json_text = JSON.stringify(body.substring(0, lastIndex+1 ) + `| ${url} | ${author} |\r\n` + body.substring(lastIndex+1, body.length))
-//     body  = JSON.parse(json_text)
-//     }
-    // console.log(JSON.stringify(bodys))
-    // let template =  JSON.stringify(bodys)
+const workMeetings =  await octokit.request('GET /repos/{owner}/{repo}/issues?labels=Working%20Meetings&per_page=1', {
+    owner: '0xSudarshan',
+    repo: 'automated-artifacts-buidl',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+  let body  = workMeetings.data[0].body
+  for (let i =0 ; i < data.length ; i++){
+    let url = data[i].html_url
+    let author = data[i].user.login
+    let search  = parseInt(JSON.stringify(body).search(url))
+    if(search === -1){
+    let startIndex  = parseInt(JSON.stringify(body.indexOf('\r\n| -- | -- | -- |\r\n'))) 
+    let lastIndex  = startIndex+19
+    let json_text = JSON.stringify(body.substring(0, lastIndex+1 ) + `| ${url} | ${author} |\r\n` + body.substring(lastIndex+1, body.length))
+    body = JSON.parse(json_text)
+    }
+    }
+    let template =  JSON.stringify(JSON.stringify(body))
     console.log("------------------------------------------------------------------")
     console.log("------------------------------------------------------------------")
     console.log("------------------------------------------------------------------")
     console.log("------------------------------------------------------------------")
-    // console.log(template)
-    let d = "degen\r\nnfts\r\ndelphi\r\nmater labs"
-    let c  = "\"degen\\r\\nnfts\\r\\ndelphi\\r\\nmater labs\""
-    let b =  JSON.parse(c);
-    let e = JSON.stringify(JSON.stringify(d))
-    let parsed   = JSON.parse(JSON.parse(e))
-    // console.log("A>>",typeof(a),a)
-    console.log("B>>",typeof(b),b)
-    console.log("E>>",typeof(e),e)
-    console.log("PARSED>>",typeof(parsed),parsed)
-
-
-    // let template = "hi\n\rhere";
+    let parsed = JSON.parse(JSON.parse(template))
 
 
 setTimeout(()=>{
