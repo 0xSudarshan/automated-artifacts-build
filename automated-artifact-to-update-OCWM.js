08740bc280aaeb7847a463d2e08c7dc62b7930c0
0xSchierke <103627996+0xSudarshan@users.noreply.github.com>
2023-02-25 23:31:52 +0530

    Update automated-artifact-to-update-OCWM.js

--- automated-artifact-to-update-OCWM.js
+++ automated-artifact-to-update-OCWM.js
@@ -2,7 +2,9 @@ import { Octokit } from '@octokit/core'
 import {getInput} from '@actions/core'
 
 const AUTH_TOKEN = getInput('AUTH_TOKEN')
+// const AUTH_TOKEN = "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"
 const ORGANISATION = getInput('ORGANISATION')
+// const ORGANISATION = "QlimesEth"
 // const REPOSITORIES  = getInput('REPOSITORIES')
 const REPOSITORIES  = ["Test-Repo-1","Test-Repo-2"]
 
