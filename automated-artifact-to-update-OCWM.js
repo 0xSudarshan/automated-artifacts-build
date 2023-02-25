5c06d74d795c4053b6998a12e60c9cfb1ffac553
0xSchierke <103627996+0xSudarshan@users.noreply.github.com>
2023-02-26 01:38:24 +0530

    v10.0

--- automated-artifact-to-update-OCWM.js
+++ automated-artifact-to-update-OCWM.js
@@ -6,55 +6,55 @@ const AUTH_TOKEN = getInput('AUTH_TOKEN')
 const ORGANISATION = getInput('ORGANISATION')
 // const ORGANISATION = "QlimesEth"
 const REPOSITORIES  = getInput('REPOSITORIES')
-
-for(let i =0;i<2;i++){
-    console.log(REPOSITORIES[i])
-}
-
+// const REPOSITORIES  = 'Test-Repo-1,Test-Repo-2'
 // const REPOSITORIES  = ["Test-Repo-1","Test-Repo-2"]
-
-// for(let r=0 ; r< REPOSITORIES.length;r++){
-//     const octokit = new Octokit({
-//         auth: AUTH_TOKEN
-//     })
-//     const {data} = await octokit.request('GET /repos/{org}/{repo}/issues?labels=agenda', {
-//         org:ORGANISATION,
-//         repo:REPOSITORIES[r],
-//       });
-//     const workMeetings = await octokit.request('GET /repos/{org}/{repo}/issues?labels=Working%20Meetings&per_page=1', {
-//         org: ORGANISATION,
-//         repo: 'Community',
-//         headers: {
-//             'X-GitHub-Api-Version': '2022-11-28'
-//         }
-//     })
-//     let body = workMeetings.data[0].body
-//     let issue_numer = workMeetings.data[0].number
-//     for (let i = 0; i < data.length; i++) {
-//         let url = data[i].html_url
-//         let author = data[i].user.login
-//         let search = parseInt(JSON.stringify(body).search(url))
-//         if (search === -1) {
-//             let startIndex = parseInt(JSON.stringify(body.indexOf('\r\n| -- | -- | -- |\r\n')))
-//             let lastIndex = startIndex + 19
-//             let json_text = JSON.stringify(body.substring(0, lastIndex + 1) + `| ${url} | ${author} |\r\n` + body.substring(lastIndex + 1, body.length))
-//             body = JSON.parse(json_text)
-//         }
-//     }
-//     let template = JSON.stringify(JSON.stringify(body))
-//     let parsed = JSON.parse(JSON.parse(template))
-//     await octokit.request('PATCH /repos/{orgs}/{repo}/issues/{issue_number}', {
-//         orgs: ORGANISATION,
-//         repo: 'Community',
-//         issue_number: `${issue_numer}`,
-//         body: parsed,
-//         milestone: null,
-//         state: 'open',
-//         headers: {
-//             'X-GitHub-Api-Version': '2022-11-28'
-//         }
-//     })
+let repositories = REPOSITORIES.split(",")
+// console.log(NEW_REPOSITORY)
+// for(let i =0;i<2;i++){
+//     console.log(NEW_REPOSITORY[i])
 // }
+for(let r=0 ; r< repositories.length;r++){
+    const octokit = new Octokit({
+        auth: AUTH_TOKEN
+    })
+    const {data} = await octokit.request('GET /repos/{org}/{repo}/issues?labels=agenda', {
+        org:ORGANISATION,
+        repo:repositories[r],
+      });
+    const workMeetings = await octokit.request('GET /repos/{org}/{repo}/issues?labels=Working%20Meetings&per_page=1', {
+        org: ORGANISATION,
+        repo: 'Community',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    let body = workMeetings.data[0].body
+    let issue_numer = workMeetings.data[0].number
+    for (let i = 0; i < data.length; i++) {
+        let url = data[i].html_url
+        let author = data[i].user.login
+        let search = parseInt(JSON.stringify(body).search(url))
+        if (search === -1) {
+            let startIndex = parseInt(JSON.stringify(body.indexOf('\r\n| -- | -- | -- |\r\n')))
+            let lastIndex = startIndex + 19
+            let json_text = JSON.stringify(body.substring(0, lastIndex + 1) + `| ${url} | ${author} |\r\n` + body.substring(lastIndex + 1, body.length))
+            body = JSON.parse(json_text)
+        }
+    }
+    let template = JSON.stringify(JSON.stringify(body))
+    let parsed = JSON.parse(JSON.parse(template))
+    await octokit.request('PATCH /repos/{orgs}/{repo}/issues/{issue_number}', {
+        orgs: ORGANISATION,
+        repo: 'Community',
+        issue_number: `${issue_numer}`,
+        body: parsed,
+        milestone: null,
+        state: 'open',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+}
 
     
 
